bd4c745317a360a8e0eb80c2ad5ee714573f960b
Saugat Tripathi <tripathi.saugat@gmail.com>
2017-06-10 18:22:51 -0400

    All files added

---
+++ routes.js
@@ -0,0 +1,11 @@
+Router.configure({
+  layoutTemplate: 'layout'
+});
+Router.route('/', function () {
+  this.render('home');
+});
+
+
+Router.route('/projects', function(){
+  this.render('projects');
+});
